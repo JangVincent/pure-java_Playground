33bb990293982ef74fb1c82d783de58b03281617
Phantola <phantoladev@gmail.com>
2024-06-03 23:36:40 +0900

    Fix folder name

--- Project/src/App.java
+++ Project/src/App.java
@@ -6,12 +6,11 @@ public class App {
         Utils console = new Utils();
 
         Person person2 = new Person("YoungJin", "Jang");
-        console.log(person2.getFullName());
 
         if (person2.getFirstName() == "John Doe") {
             throw new Exception("Person is not Initialized");
         }
 
-        console.log("He is John Doe");
+        console.log(person2.getFullName());
     }
 }
