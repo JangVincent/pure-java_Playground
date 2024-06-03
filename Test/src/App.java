912580461d0d77d2e96bf2f5d4b4c3c136f9cfa9
Phantola <phantoladev@gmail.com>
2024-06-03 23:13:01 +0900

    Fix App.java

--- Test/src/App.java
+++ Test/src/App.java
@@ -2,17 +2,15 @@ import classes.Person;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person();
-        System.out.println(person.getFullName());
 
         Person person2 = new Person("YoungJin", "Jang");
         System.out.println(person2.getFirstName() + " " + person2.getLastName());
 
 
-        if(person.getFirstName() == "John Doe") {
-            System.out.println("He is John Doe");
-        } else {
+        if(person2.getFirstName() == "John Doe") {
             throw new Exception("Person is not Initialized");
-        }
+        } 
+
+        System.out.println("He is John Doe");
     }
 }
