a4fc7799c5030a2fbffa4b87f229200a0b311907
Phantola <phantoladev@gmail.com>
2024-06-03 23:51:34 +0900

    Add inheritance Test

--- Playground/src/App.java
+++ Playground/src/App.java
@@ -1,4 +1,5 @@
 import classes.Person;
+import classes.Programmer;
 import classes.Utils;
 
 public class App {
@@ -11,6 +12,26 @@ public class App {
             throw new Exception("Person is not Initialized");
         }
 
-        console.log(person2.getFullName());
+        // For Inheritance Test
+
+        Programmer prog1 = new Programmer(person2, "Java", "Backend Engineer", "CatzeLabs", 3);
+
+        console.log(prog1.getFullName());
+
+        if (prog1.getFullName() == "John Doe") {
+            throw new Exception("Programmer name is not Initialized");
+        }
+
+        console.log(prog1.getFormattedIntroduction() + '\n');
+
+        // Change most used Language
+        prog1.setMostUseLanguage("Javascript/Typescript");
+
+        if (prog1.getMostUseLanguage() == "Java") {
+            throw new Exception("Most used language is not changed");
+        }
+
+        console.log(prog1.getFormattedIntroduction());
+
     }
 }
---
+++ Playground/src/classes/Programmer.java
@@ -0,0 +1,66 @@
+package classes;
+
+public class Programmer extends Person {
+  String mostUseLanguage;
+  String role;
+  String company;
+  int careerYears;
+
+  public Programmer() {
+    super();
+  }
+
+  public Programmer(String firstName, String lastName, String mostUseLanguage, String role, String company,
+      int careerYears) {
+    super(firstName, lastName);
+    this.mostUseLanguage = mostUseLanguage;
+    this.role = role;
+    this.company = company;
+    this.careerYears = careerYears;
+  }
+
+  public Programmer(Person person, String mostUseLanguage, String role, String company, int careerYears) {
+    super(person.getFirstName(), person.getLastName());
+    this.mostUseLanguage = mostUseLanguage;
+    this.role = role;
+    this.company = company;
+    this.careerYears = careerYears;
+  }
+
+  public String getFormattedIntroduction() {
+    return String.format("Hello, I'm %s. I'm a %s at %s.\nI have %d years of experience in %s.", this.getFullName(),
+        this.role, this.company, this.careerYears, this.mostUseLanguage);
+  }
+
+  public String getMostUseLanguage() {
+    return this.mostUseLanguage;
+  }
+
+  public void setMostUseLanguage(String mostUseLanguage) {
+    this.mostUseLanguage = mostUseLanguage;
+  }
+
+  public String getRole() {
+    return this.role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getCompany() {
+    return this.company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public int getCareerYears() {
+    return this.careerYears;
+  }
+
+  public void setCareerYears(int careerYears) {
+    this.careerYears = careerYears;
+  }
+}
