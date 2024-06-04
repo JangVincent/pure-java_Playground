98b433d083a110a66941eca944424bb469290893
Phantola <phantoladev@gmail.com>
2024-06-04 19:30:37 +0900

    Fix chore

--- Playground/src/App.java
+++ Playground/src/App.java
@@ -7,14 +7,12 @@ public class App {
         Utils console = new Utils();
 
         Person person2 = new Person("YoungJin", "Jang");
-
         if (person2.getFirstName().equals("John Doe")) {
             throw new Exception("Person is not Initialized");
         }
 
         // For Inheritance Test
         Programmer prog1 = new Programmer(person2, "Java", "Backend Engineer", "CatzeLabs", 1);
-
         console.log(prog1.getFullName());
 
         if (prog1.getFullName().equals("John Doe")) {
@@ -26,17 +24,16 @@ public class App {
         // Change most used Language
         prog1.setMostUseLanguage("Javascript/Typescript");
         prog1.setCareerYears(3);
-
         if (prog1.getMostUseLanguage().equals("Java")) {
             throw new Exception("Most used language is not changed");
         }
 
         console.log(prog1.getFormattedIntroduction() + '\n');
-
         // Try-catch block
         try {
             Programmer prog2 = new Programmer(new Person(), "Java", "Backend Engineer", "CatzeLabs", 3);
-            Person programmerPerson = prog2.getPerson();
+            Person personOfProg2 = prog2.getPerson();
+            personOfProg2.getFullName();
         } catch (Exception e) {
             console.log(e.getMessage());
         }
